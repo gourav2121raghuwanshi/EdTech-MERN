1ef54088978d331c9c60e2fd396ee6c551c3a312
gourav2121raghuwanshi <7400563257gourav@gmail.com>
2024-06-05 21:58:37 +0530

    mongodb database connected

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,11 @@ const express=require('express')
 const app= express()
 const dotenv = require("dotenv");
 dotenv.config();
-
+const database = require("./config/database");
 const PORT = process.env.PORT || 4000;
 
 
+database.connect();
 app.get("/", (req, res) => {
 	return res.json({
 		success:true,
