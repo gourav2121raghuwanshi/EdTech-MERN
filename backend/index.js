309b532140d976872182f6fbcbd13046443fc044
gourav2121raghuwanshi <7400563257gourav@gmail.com>
2024-06-06 13:44:33 +0530

    backend completed

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,12 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const { cloudinaryConnect } = require("./config/cloudinary");
 const fileUpload = require("express-fileupload");
+const userRoutes = require("./routes/User");
+const profileRoutes = require("./routes/Profile");
+const paymentRoutes = require("./routes/Payments");
+const courseRoutes = require("./routes/Course");
+const contactUsRoute = require("./routes/Contact");
+
 database.connect();
 app.use(express.json());
 app.use(cookieParser());
@@ -26,6 +32,14 @@ app.use(
 )
 
 cloudinaryConnect();
+
+//routes
+app.use("/api/v1/auth", userRoutes);
+app.use("/api/v1/profile", profileRoutes);
+app.use("/api/v1/course", courseRoutes);
+app.use("/api/v1/payment", paymentRoutes);
+app.use("/api/v1/reach", contactUsRoute);
+
 
 app.get("/", (req, res) => {
 	return res.json({
