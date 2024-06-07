94915163590cda34c9011b663dc7ab2eb212c560
gourav2121raghuwanshi <7400563257gourav@gmail.com>
2024-06-07 21:55:53 +0530

    backend url change

--- frontend/src/services/apis.js
+++ frontend/src/services/apis.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "http://localhost:4000/api/v1"
-const BASE_URL = "https://ed-tech-mern-backend.vercel.app"
+const BASE_URL = "https://ed-tech-mern-backend.vercel.app/api/v1"
 
 // AUTH ENDPOINTS
 export const endpoints = {
